4dea70024411b745d97a569ba1aa3c6e4553264b
Chantal Justamond <cjustamm66@me.com>
2017-05-18 09:15:15 -0600

    feature tests playlist - iteration 4 is wip :)

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+
   def new
     @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.new
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,9 +1,28 @@
+# FactoryGirl.define do
+#   factory :artist do
+#     sequence :name do |n|
+#       "#{n}Bob Marley"
+#     end
+#     image_path "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
+#   end
+#
+# end
+
 FactoryGirl.define do
   factory :artist do
-    sequence :name do |n|
-      "#{n}Bob Marley"
-    end
+    name
     image_path "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
   end
+  sequence :name do |n|
+      "#{n} Artist"
+  end
 
+  sequence :title, ["A", "C", "B"].cycle do |n|
+    "#{n} Title"
+  end
+
+  factory :song do
+    title
+    artist
+  end
 end
